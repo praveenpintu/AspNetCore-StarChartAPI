71ff748d1a4140da895f10b46c2e621d8267b7e9
Praveen-PraveenN <nallatigalapraveen1234@gmail.com>
2022-02-09 12:26:59 +0530

    get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,5 +18,38 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var entity =_context.CelestialObjects.Find(id);
+            if (entity == null)
+                return NotFound();
+            entity.Satellites = _context.CelestialObjects.Where(x => x.Id == id).ToList();
+            return Ok(entity);
+        }
+
+        [HttpGet("{name}", Name = "GetByName ")]
+        public IActionResult GetByName(string name)
+        {
+            var entity = _context.CelestialObjects.Find(name);
+            if (entity == null)
+                return NotFound();
+            entity.Satellites = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            return Ok(entity);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var entity = _context.CelestialObjects.ToList();
+            foreach (var celestialObject in entity)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(entity);
+        }
+
+       
     }
 }
